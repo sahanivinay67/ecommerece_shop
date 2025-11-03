887f7634d9f32bd151faa386f78d81d7c3f4d765
vinay sahani <vinay@vinays-MacBook-Air.local>
2025-11-03 22:30:30 +0530

    bloc and cubit tutorials

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:ecommerece_shop/bloc/counter_bloc.dart';
+import 'package:ecommerece_shop/cubit/counter_cubit.dart';
 import 'package:ecommerece_shop/future_builder.dart';
+import 'package:ecommerece_shop/inc_dec.dart';
 import 'package:ecommerece_shop/isolate_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,12 +16,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => CounterCubit()),
+        BlocProvider(create: (context) => CounterBloc()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: IncDec(),
       ),
-      home: Futurebuilder(),
     );
   }
 }
